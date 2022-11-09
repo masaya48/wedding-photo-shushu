75ef8153879efbe8b7488df084f248c0a60ac43c
masaya48 <msya0408@gmail.com>
2022-11-09 23:04:47 +0900

    add privacypolicy

---
+++ src/pages/privacypolicy/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import { Container } from '@mui/material'
+
+
+const PrivacyPolicy: NextPage = () => (
+  <Container className="p-4 font-mono bg-[url('/bg-main.jpg')] bg-cover bg-no-repeat h-[100vh] bg-[top_left_85%]">
+    <section>
+      <h2>個人情報の取り扱いについて</h2>
+    </section>
+    <section>
+      <h2>肖像権・著作権について</h2>
+    </section>
+  </Container>
+)
+
+export default PrivacyPolicy
